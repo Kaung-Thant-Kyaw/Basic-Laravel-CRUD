83a82c9ad773d34305debdf053e71480eac22b7f
Kaung Thant Kyaw <kaungthantkyaw112497@gmail.com>
2024-05-26 21:49:39 +0630

    create table to display posts data

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,3 +1,36 @@
 <x-template>
-  <h1>Hello, I am index</h1>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-2"></div>
+      <div class="col-md-8">
+        <h5 class="text-center mb-3">Posts List</h5>
+          <table class="table table-bordered table-hover shadow-md">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Title</th>
+                <th>Content</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>PHP</td>
+                <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. </td>
+                <td>
+                  <button class="btn btn-warning btn-sm">
+                    <i class="fa fa-edit"></i> Edit
+                  </button>
+                  <button class="btn btn-danger btn-sm">
+                    <i class="fa fa-trash"></i> Delete
+                  </button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+      </div>
+      <div class="col-md-2"></div>
+    </div>
+  </div>
 </x-template>
